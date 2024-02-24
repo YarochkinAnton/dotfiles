78b8e3cee5bd7fe3b7fef08e0875fb994f493c86
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-02-24 10:19:07 +0300

    Disable auto formatting for json files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,12 @@
--- au BufRead,BufNewFile *.bu set filetype=yaml
-
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.bu",
-    command = "set filetype=yaml"
+	pattern = "*.bu",
+	command = "set filetype=yaml",
+})
+
+-- Disable autoformat for json files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "json" },
+	callback = function()
+		vim.b.autoformat = false
+	end,
 })
