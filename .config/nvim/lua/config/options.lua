e5e71775f0c23a462283c9ebd79faaa973b01886
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-02-27 04:02:20 +0300

    Formatting

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.confirm = false
-vim.opt.guicursor = 'i:block'
+vim.opt.guicursor = "i:block"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.g.material_style = 'deep ocean'
+vim.g.material_style = "deep ocean"
