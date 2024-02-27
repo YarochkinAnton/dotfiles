94062fd558800b371e4dd12a4d05598c85fe4448
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-02-27 04:02:42 +0300

    Added ansible syntax detection

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -3,6 +3,11 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	command = "set filetype=yaml",
 })
 
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*ansible.yaml" },
+	command = "set filetype=yaml.ansible",
+})
+
 -- Disable autoformat for json files
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "json" },
