93cf6ba9a9f2d544145f9e26e576961de3d3294e
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-02-27 06:39:32 +0300

    Whoops

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,18 +15,6 @@ require("lazy").setup({
 		-- { import = "lazyvim.plugins.extras.lang.json" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
-		{
-			"neovim/nvim-lspconfig",
-			---@class PluginLspOpts
-			opts = {
-				---@type lspconfig.options
-				servers = {
-					terraformls = {
-						mason = false,
-					},
-				},
-			},
-		},
 		{ import = "plugins" },
 	},
 	defaults = {
