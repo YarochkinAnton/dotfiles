ce9572d6744bde6e8a69ddfeb05e854cfab00a71
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-04 20:17:30 +0300

    Added borders for completion plugin windows

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,14 @@
+return {
+	"hrsh7th/nvim-cmp",
+	opts = {
+		window = {
+			documentation = {
+				border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+			},
+			completion = {
+				border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+				winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+			},
+		},
+	},
+}
