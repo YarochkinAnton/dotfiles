c149044a1f9a42054d62b58bbf86de45623cd482
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-04 20:19:00 +0300

    Enabled inlay hints in nvim-lspconfig

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,19 @@
 return {
 	"neovim/nvim-lspconfig",
 	opts = {
+		inlay_hints = {
+			enabled = true,
+		},
 		servers = {
 			terraformls = {
 				mason = false, -- this is required due to not being able to download this language server
+			},
+			lua_ls = {
+				Lua = {
+					hint = {
+						enable = true,
+					},
+				},
 			},
 		},
 	},
