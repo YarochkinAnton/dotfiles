baf5157fe7e20b7842dba7d0db551099f7e538c1
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-08 18:25:53 +0300

    Added keymaps for easy nvim configuration changes

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,32 @@ vim.keymap.set("", "<S-Tab>", "gT")
 
 -- Enter colon with double-tap semicolon
 vim.keymap.set("n", ";;", ":", { desc = "which_key_ignore" })
+
+-- Search in my dotfiles
+vim.keymap.set("n", "<leader>fdf", function()
+	local dotfiles_dir = os.getenv("DOTFILES")
+	if nil == dotfiles_dir then
+		---@diagnostic disable-next-line: missing-fields
+		require("notify").notify("DOTFILES environment variable is not set", "error", {
+			title = " Dotfile search FAILED",
+		})
+	else
+		require("telescope.builtin").git_files({
+			cwd = dotfiles_dir,
+		})
+	end
+end, { desc = "Find Dotfile" })
+
+vim.keymap.set("n", "<leader>fdg", function()
+	local dotfiles_dir = os.getenv("DOTFILES")
+	if nil == dotfiles_dir then
+		---@diagnostic disable-next-line: missing-fields
+		require("notify").notify("DOTFILES environment variable is not set", "error", {
+			title = " Dotfile search FAILED",
+		})
+	else
+		require("telescope.builtin").live_grep({
+			cwd = dotfiles_dir,
+		})
+	end
+end, { desc = "Grep Dotfiles" })
