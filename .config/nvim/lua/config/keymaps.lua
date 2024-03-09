60e265f73208ff47f9346e8637a65ba99a8be886
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-09 18:21:12 +0300

    Disabled insert mode double tap escape
    I've got myself some new fancy keyboard with thumb keys

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Exiting insert mode without Esc
-vim.keymap.set("i", "ii", "<Esc>", { silent = true, desc = "which_key_ignore" })
-vim.keymap.set("i", "шш", "<Esc>")
+-- vim.keymap.set("i", "ii", "<Esc>", { silent = true, desc = "which_key_ignore" })
+-- vim.keymap.set("i", "шш", "<Esc>")
 
 -- Tab switching
 vim.keymap.set("", "<Tab>", "gt")
