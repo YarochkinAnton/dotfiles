1cf679265ed45461568a79cebdeaeaa8573f569b
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-24 00:24:38 +0300

    Added configuration for nix home-manager

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,131 @@
+{ config, pkgs, ... }:
+
+{
+    imports = [
+        ./services.nix
+    ];
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "poplav";
+    home.homeDirectory = "/home/poplav";
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "23.11"; # Please read the comment before changing.
+
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    home.packages = [
+      # # Adds the 'hello' command to your environment. It prints a friendly
+      # # "Hello, world!" when run.
+      # pkgs.hello
+
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
+        pkgs.htop
+        pkgs.vlc
+        pkgs.ansible
+        pkgs.ansible-language-server
+        pkgs.ansible-lint
+        pkgs.bat
+        pkgs.borgbackup
+        pkgs.butane # For coreos configuration conversion
+        pkgs.cilium-cli
+        pkgs.dust # du + rust
+        pkgs.fd
+        pkgs.freerdp
+        # TODO: https://github.com/unixorn/fzf-zsh-plugin
+        pkgs.fzf
+        pkgs.gimp
+        pkgs.go
+        pkgs.pandoc
+        pkgs.kubernetes-helm
+        pkgs.ranger
+        # TODO: fix glfw error
+        pkgs.imhex
+        pkgs.hyprshot
+        pkgs.jq
+        pkgs.lazygit
+        pkgs.mako
+        pkgs.musescore
+        pkgs.nodejs
+        # TODO: Apply this non-NixOS fix
+        # https://github.com/ch1bo/dotfiles/blob/c17d07e783f71988b9be27185f94b75667c8b9f6/hosts/matterhorn/default.nix#L10
+        pkgs.obs-studio
+        pkgs.pavucontrol
+        pkgs.pulsemixer
+        pkgs.inkscape
+        pkgs.rdesktop
+        pkgs.ripgrep
+        pkgs.rofi-wayland
+        pkgs.rsync
+        pkgs.step-cli
+        pkgs.telegram-desktop
+        pkgs.tldr
+        pkgs.tokei
+        pkgs.ueberzugpp
+        pkgs.unzip
+        pkgs.zip
+        pkgs.k9s
+        pkgs.transmission
+        pkgs.udisks
+        pkgs.e2fsprogs
+        pkgs.dosfstools
+        (pkgs.python3.withPackages (python-pkgs: [
+            python-pkgs.jinja2
+        ]))
+    ];
+
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    home.file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. If you don't want to manage your shell through Home
+    # Manager then you have to manually source 'hm-session-vars.sh' located at
+    # either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/poplav/etc/profile.d/hm-session-vars.sh
+    #
+    home.sessionVariables = {
+      # EDITOR = "emacs";
+      # LC_ALL = "ru_RU.UTF-8";
+    };
+
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
+}
