cd387e70ccffeed1fa8560c9cf1e4573ac911a14
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-26 21:40:32 +0300

    Added wrapping to alacritty due to it being installed with nix

---
+++ bin/alacritty.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# nixGL wrapper script
+# Sole puprose of this script is to invoke alacritty with nixGL due to problems running nix GUI apps on non nixOS distors
+
+nixGL alacritty $@
