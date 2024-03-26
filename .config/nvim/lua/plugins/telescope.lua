be81005fe3a30342244c4c6ea4c1ff6aa5667734
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-26 23:03:49 +0300

    Apparently there were tabs, yuck

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,16 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	opts = {
-		pickers = {
-			find_files = {
-				hidden = true,
-			},
-			grep_string = {
-				additional_args = { "--hidden" },
-			},
-			live_grep = {
-				additional_args = { "--hidden" },
-			},
-		},
-	},
+    "nvim-telescope/telescope.nvim",
+    opts = {
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
+            grep_string = {
+                additional_args = { "--hidden" },
+            },
+            live_grep = {
+                additional_args = { "--hidden" },
+            },
+        },
+    },
 }
