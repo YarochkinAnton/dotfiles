ee387ece6fd5032062de6fe21215fcb096e24125
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-03-27 03:26:38 +0300

    Reconfigured telescope nvim plugin
    - Configured flex layout to accommodate for focus mode
    - Added key binding to toggle preview
    - Used specific config parameter to ignore `.git` folder when
      using pickers instead of providing plain arguments

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local actions_layout = require("telescope.actions.layout")
+
 return {
     "nvim-telescope/telescope.nvim",
     opts = {
@@ -9,7 +11,27 @@ return {
                 additional_args = { "--hidden" },
             },
             live_grep = {
-                additional_args = { "--hidden", "--glob", "!**/.git/**" },
+                additional_args = { "--hidden" },
+            },
+        },
+        defaults = {
+            file_ingore_patterns = ".git",
+            mappings = {
+                n = {
+                    ["p"] = actions_layout.toggle_preview,
+                },
+            },
+            preview = {
+                -- hide_on_startup = true,
+            },
+            layout_strategy = "flex",
+            layout_config = {
+                flex = {
+                    flip_columns = 150,
+                },
+                horizontal = {
+                    preview_width = 0.6,
+                },
             },
         },
     },
