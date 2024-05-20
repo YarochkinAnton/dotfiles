5f139e66f8f59e9199cbfcbbb17c826fdbfb01fa
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-05-20 03:21:32 +0300

    Trying telescope-file-browser instead of neo-tree

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,17 @@ local actions_layout = require("telescope.actions.layout")
 
 return {
     "nvim-telescope/telescope.nvim",
+    lazy = false,
+    dependencies = {
+        {
+            "nvim-telescope/telescope-file-browser.nvim",
+            config = function(plugin)
+                LazyVim.on_load("telescope.nvim", function()
+                    pcall(require("telescope").load_extension, "file_browser")
+                end)
+            end,
+        },
+    },
     opts = {
         pickers = {
             find_files = {
@@ -29,5 +40,23 @@ return {
                 },
             },
         },
+        extensions = {
+            file_browser = {
+                -- theme = "ivy",
+                -- disables netrw and use telescope-file-browser in its place
+                hijack_netrw = true,
+                mappings = {
+                    i = {
+                        -- your custom insert mode mappings
+                    },
+                    n = {
+                        -- your custom normal mode mappings
+                    },
+                },
+            },
+        },
+    },
+    keys = {
+        { "<leader>e", "<cmd>Telescope file_browser path=. hidden=true<CR>" },
     },
 }
