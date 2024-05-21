8e4ca6051ee9a91c55c32b49be739f46c1287d09
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-05-21 23:13:14 +0300

    Some nvim cleanup

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,6 @@
 return {
     { "marko-cerovac/material.nvim" },
     { "rose-pine/neovim", name = "rose-pine" },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-    },
     { "shaunsingh/nord.nvim" },
     { "neanias/everforest-nvim" },
     {
