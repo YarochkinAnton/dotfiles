74daaddb2d90f9ca51b3746e4cf539a9c8ad19cd
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-05-27 01:48:54 +0300

    Migrated to Nix Flakes
    Due to [this](https://github.com/nix-community/neovim-nightly-overlay/issues/530)
    issue decided that migrating to flakes is better for long-term
    as it seems that everybody is using them.

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -111,12 +111,6 @@
         enable = true;
         systemd.enable = true;
     };
-    #
-    # nixpkgs.overlays = [
-    #     (import (builtins.fetchTarball {
-    #         url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-    #     }))
-    # ];
     programs.neovim = {
         enable = true;
         defaultEditor = true;
