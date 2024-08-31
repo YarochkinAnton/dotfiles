628d76da689478d55479c947bb2ba9f7bac2fa56
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-08-31 17:34:06 +0300

    Finally read docs for langmapper

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,3 @@
 require("config.lazy")
 require("config.filetype")
-require("langmapper").automapping()
+require("langmapper").automapping({ global = true, buffer = true })
--- .config/nvim/lua/plugins/langmapper.lua
+++ .config/nvim/lua/plugins/langmapper.lua
@@ -2,4 +2,7 @@ return {
     "Wansmer/langmapper.nvim",
     lazy = false,
     priority = 1, -- High priority is needed if you will use `autoremap()`
+    config = function()
+        require("langmapper").setup()
+    end,
 }
