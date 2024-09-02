39c89fbd2cc19b7bbe824fe645124d6bfa9398b7
Yarochkin Anton <yarochkin.anton@yandex.ru>
2024-09-02 21:18:59 +0300

    Updated markdown linter config

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -2,11 +2,6 @@ return {
     {
         "mfussenegger/nvim-lint",
         opts = {
-            linters = {
-                markdownlint = {
-                    args = { "--config", "~/.config/markdownlint.json", "--" },
-                },
-            },
             linters_by_ft = {
                 markdown = { "markdownlint" },
             },
