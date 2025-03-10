087f5e979abf325db32b6f9cf8fc19a641068706
Yarochkin Anton <yarochkin.anton@yandex.ru>
2025-03-10 08:38:18 +0300

    Changed theme to gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,11 @@ return {
     { "arcticicestudio/nord-vim", name = "nord" },
     { "EdenEast/nightfox.nvim" },
     { "catppuccin/nvim", name = "catppuccin" },
+    { "ellisonleao/gruvbox.nvim" },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "catppuccin-mocha",
+            colorscheme = "gruvbox",
         },
     },
 }
