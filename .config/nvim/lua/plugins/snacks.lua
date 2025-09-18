1151bda27040007cc549367d19dda835ce0c6c73
Yarochkin Anton <yarochkin.anton@yandex.ru>
2025-09-18 03:12:30 +0300

    Configured lazygit so it will take more screen space

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/snacks.nvim",
+    opts = {
+        lazygit = {
+            win = {
+                style = "float",
+                relative = "editor",
+                width = 220,
+                height = 100,
+                row = 0,
+                col = 0,
+            },
+        },
+    },
+}
