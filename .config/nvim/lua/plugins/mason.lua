16b53dcd4895f9c7e201e6e79630bc09f5bc2c57
Yarochkin Anton <yarochkin.anton@yandex.ru>
2025-10-30 20:03:44 +0300

    Updated mason border

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
         "mason-org/mason.nvim",
         opts = {
             ui = {
-                border = "rounded",
+                border = "solid",
             },
         },
     },
